3d775b6ba604f75461137b82f09a3df5b4ee1f87
loveemu <loveemu@gmail.com>
2019-06-21 00:07:50 +0900

    Add source pattern files

---
+++ src/arm/make.bat
@@ -0,0 +1,11 @@
+@echo off
+
+sigmake -n"AGB Library: AGB Game Pak Backup Library [libagbbackup.a]" libagbbackup.pat libagbbackup.sig
+sigmake -n"AGB Library: AGB IR Communication Library [libagbir.a]" libagbir.pat libagbir.sig
+sigmake -n"AGB Library: AGB System Call [libagbsyscall.a]" libagbsyscall.pat libagbsyscall.sig
+sigmake -n"AGB Library: IS-AGB-EMULATOR PRINT DEBUG LIBRARY [libisagbprn.a]" libisagbprn.pat libisagbprn.sig
+
+sigmake -n"AGB Music Player 2000 (DMA Limit) [m4aLibOD1.a]" m4aLibOD1.pat m4aLibOD1.sig
+sigmake -n"AGB Music Player 2000 [mks4agbLib.a]" mks4agbLib.pat mks4agbLib.sig
+
+for %%a in (*.sig) do zipsig "%%a"
