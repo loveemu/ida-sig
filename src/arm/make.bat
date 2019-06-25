60468e80e613d2bb507196039482aff18ebcfc3a
loveemu <loveemu@gmail.com>
2019-06-26 01:28:49 +0900

    Change signature description of libgcc_agb.sig a little

--- src/arm/make.bat
+++ src/arm/make.bat
@@ -9,6 +9,6 @@ sigmake -n"AGB Music Player 2000 (DMA Limit) [m4aLibOD1.a]" m4aLibOD1.pat m4aLib
 sigmake -n"AGB Music Player 2000 [mks4agbLib.a]" mks4agbLib.pat mks4agbLib.sig
 sigmake -n"AGB Music Player 2000 (Later Version)" mks4agbLib.2.pat mks4agbLib.2.sig
 
-sigmake -n"AGB Library: GCC Low-Level Runtime Library [libgcc.a]" libgcc_agb.pat libgcc_agb.sig
+sigmake -n"AGB Developer Toolkit: GCC Low-Level Runtime Library [libgcc.a]" libgcc_agb.pat libgcc_agb.sig
 
 for %%a in (*.sig) do zipsig "%%a"
