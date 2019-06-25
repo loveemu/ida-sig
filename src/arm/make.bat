5623f40ee82d95ef4463f4aa236ada64f763f917
loveemu <loveemu@gmail.com>
2019-06-26 01:09:18 +0900

    Rename mks4agbLib2.sig to mks4agbLib.2.sig for no particular reason

--- src/arm/make.bat
+++ src/arm/make.bat
@@ -7,7 +7,7 @@ sigmake -n"AGB Library: IS-AGB-EMULATOR PRINT DEBUG LIBRARY [libisagbprn.a]" lib
 
 sigmake -n"AGB Music Player 2000 (DMA Limit) [m4aLibOD1.a]" m4aLibOD1.pat m4aLibOD1.sig
 sigmake -n"AGB Music Player 2000 [mks4agbLib.a]" mks4agbLib.pat mks4agbLib.sig
-sigmake -n"AGB Music Player 2000 (Later Version)" mks4agbLib2.pat mks4agbLib2.sig
+sigmake -n"AGB Music Player 2000 (Later Version)" mks4agbLib.2.pat mks4agbLib.2.sig
 
 sigmake -n"AGB Library: GCC Low-Level Runtime Library [libgcc.a]" libgcc_agb.pat libgcc_agb.sig
 
